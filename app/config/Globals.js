fd9d73b1aa8f9bdd14e64d0711b32875277e19b0
Ubuntu <ubuntu@ip-172-31-9-191.us-west-2.compute.internal>
2017-06-26 03:21:20 +0000

    Add old server_url

--- app/config/Globals.js
+++ app/config/Globals.js
@@ -1,3 +1,4 @@
 export const pages_url = 'https://s3-us-west-2.amazonaws.com/paraform/pages'
-export const server_url = 'http://127.0.0.1:5000/api'
+// export const server_url = 'http://127.0.0.1:5000/api'
+export const server_url = 'http://topielski.com/api'
 export const current_version = '1.0'
